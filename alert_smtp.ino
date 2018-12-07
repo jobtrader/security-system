767e6dbb753b31740864a14106eea67c3f85739f
Natakorn Thongdee <job.masker@gmail.com>
2018-12-07 18:47:53 +0700

    first commit

---
+++ alert_smtp.ino
@@ -0,0 +1,153 @@
+#include <ESP8266WiFi.h>
+const char* ssid = "CPRE_Wifi"; // Enter the SSID of your WiFi Network.
+const char* password = "w=piy9ot";// Enter the Password of your WiFi Network.
+char server[] = "mail.smtp2go.com"; // The SMTP Server 
+
+WiFiClient espClient;
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+  Serial.println("");
+  Serial.println("");
+  Serial.print("Connecting To: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print("*");
+  }
+  Serial.println("");
+  Serial.println("WiFi Connected.");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  byte ret = sendEmail();
+ }
+
+void loop()
+{
+  
+}
+
+byte sendEmail()
+{
+  if (espClient.connect(server, 2525) == 1) 
+  {
+    Serial.println(F("connected"));
+  } 
+  else 
+  {
+    Serial.println(F("connection failed"));
+    return 0;
+  }
+  if (!emailResp()) 
+    return 0;
+  //
+  Serial.println(F("Sending EHLO"));
+  espClient.println("EHLO www.example.com");
+  if (!emailResp()) 
+    return 0;
+  //
+  /*Serial.println(F("Sending TTLS"));
+  espClient.println("STARTTLS");
+  if (!emailResp()) 
+  return 0;*/
+  //  
+  Serial.println(F("Sending auth login"));
+  espClient.println("AUTH LOGIN");
+  if (!emailResp()) 
+    return 0;
+  //  
+  Serial.println(F("Sending User"));
+  // Change this to your base64, ASCII encoded username
+  /*
+  For example, the email address test@gmail.com would be encoded as dGVzdEBnbWFpbC5jb20=
+  */
+  espClient.println("dGVzdEBnbWFpbC5jb20="); //base64, ASCII encoded Username
+  if (!emailResp()) 
+    return 0;
+  //
+  Serial.println(F("Sending Password"));
+  // change to your base64, ASCII encoded password
+  /*
+  For example, if your password is "testpassword" (excluding the quotes),
+  it would be encoded as dGVzdHBhc3N3b3Jk
+  */
+  espClient.println("dGVzdHBhc3N3b3Jk");//base64, ASCII encoded Password
+  if (!emailResp()) 
+    return 0;
+  //
+  Serial.println(F("Sending From"));
+  // change to sender email address
+  espClient.println(F("MAIL From: s5901012620169@email.kmutnb.ac.th"));
+  if (!emailResp()) 
+    return 0;
+  // change to recipient address
+  Serial.println(F("Sending To"));
+  espClient.println(F("RCPT To: job.masker@gmail.com"));
+  if (!emailResp()) 
+    return 0;
+  //
+  Serial.println(F("Sending DATA"));
+  espClient.println(F("DATA"));
+  if (!emailResp()) 
+    return 0;
+  Serial.println(F("Sending email"));
+  // change to recipient address
+  espClient.println(F("To:  job.masker@gmail.com"));
+  // change to your address
+  espClient.println(F("From: s5901012620169@email.kmutnb.ac.th"));
+  espClient.println(F("Subject: System Alert Box\r\n"));
+  espClient.println(F("Alert: Security box\n"));
+  espClient.println(F("Your accomodation is under invasion."));
+  espClient.println(F("Please comeback to your accomodation."));
+  //
+  espClient.println(F("."));
+  if (!emailResp()) 
+    return 0;
+  //
+  Serial.println(F("Sending QUIT"));
+  espClient.println(F("QUIT"));
+  if (!emailResp()) 
+    return 0;
+  //
+  espClient.stop();
+  Serial.println(F("disconnected"));
+  return 1;
+}
+
+byte emailResp()
+{
+  byte responseCode;
+  byte readByte;
+  int loopCount = 0;
+
+  while (!espClient.available()) 
+  {
+    delay(1);
+    loopCount++;
+    // Wait for 20 seconds and if nothing is received, stop.
+    if (loopCount > 20000) 
+    {
+      espClient.stop();
+      Serial.println(F("\r\nTimeout"));
+      return 0;
+    }
+  }
+
+  responseCode = espClient.peek();
+  while (espClient.available())
+  {
+    readByte = espClient.read();
+    Serial.write(readByte);
+  }
+
+  if (responseCode >= '4')
+  {
+    //  efail();
+    return 0;
+  }
+  return 1;
+}
